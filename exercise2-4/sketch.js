e18be63564833987ae622c2cfbf79821573f72bd
keiral7187 <kvx521@york.ac.uk>
2025-10-16 11:38:57 +0100

    Update sketch.js

--- exercise2-4/sketch.js
+++ exercise2-4/sketch.js
@@ -6,7 +6,7 @@ function setup() {
 function draw() {
     background(0);
     for (let x = 0; x < width; x += 50) {
-        for (let y = 0; y < height; y += 50) {
+        for (let y = 0; y < height; y += 50) { 
             square(x, y, 50);
         }
     }

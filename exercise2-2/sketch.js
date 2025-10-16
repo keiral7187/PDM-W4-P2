fe407cb5b1f54908593fb8528ab7ca045535a3fb
keiral7187 <kvx521@york.ac.uk>
2025-10-16 11:34:32 +0100

    done 2.3

---
+++ exercise2-2/sketch.js
@@ -0,0 +1,15 @@
+function setup(){
+    createCanvas(400,400)
+}
+
+function draw(){
+    background(255);
+    for(let x = 0; x<400;x+=50){
+        rect(x,0,50,400)
+    }
+}
+
+function isRectSelected(){
+    if(mouseIsPressed && )
+
+}
---
+++ exercise2-3/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+}
+ 
+ 
+function draw() {
+    background(0);
+    for (let x = 0; x < width; x += 50) {
+        for (let y = 0; y < height; y += 50) {
+            square(x, y, 50);
+        }
+    }
+}

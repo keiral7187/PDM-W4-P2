b484d355c8a4e8627eae4e0a6c623773ad6e8b39
keiral7187 <kvx521@york.ac.uk>
2025-10-16 10:55:49 +0100

    done 1.2

---
+++ exercise1-2/sketch.js
@@ -0,0 +1,18 @@
+function setup(){
+    createCanvas(400,400);
+}
+
+function draw(){
+    background(255);
+    let x = 10;
+    while (x<400){
+        fill(0,0,255);
+        circle(x,200,20)
+        x+=20
+    }
+    while (x>mouseX){
+        fill(255);
+        circle(x,200,20)
+        x-=20
+    }
+}

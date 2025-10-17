fc6925fd8982e262647573cefc754ce1b40a28b3
keiral7187 <kvx521@york.ac.uk>
2025-10-17 11:32:00 +0100

    done 2.2

--- exercise2-2/sketch.js
+++ exercise2-2/sketch.js
@@ -1,15 +1,25 @@
+let fillColour = 255;
+
 function setup(){
     createCanvas(400,400)
 }
 
 function draw(){
     background(255);
-    for(let x = 0; x<400;x+=50){
-        rect(x,0,50,400)
+    let x = 0 
+    while (x < width){
+        fill(255);
+        rect(x, 0, 50, height);
+        if(mouseX >= x && mouseX <= x + 50){
+            if(mouseIsPressed){
+                fillColour = color(0,0,255);
+                fill(fillColour)
+                rect(x,0,50,height);
+            } else {
+                fillColour = (255);
+            }
+    }
+    x += 50;
     }
 }
 
-function isRectSelected(){
-    if(mouseIsPressed && )
-
-}

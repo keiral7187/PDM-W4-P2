0e1bc8e929694de1847d7c09566ebdcd3adb9a6c
keiral7187 <kvx521@york.ac.uk>
2025-10-17 11:35:15 +0100

    done 2.4

--- exercise2-4/sketch.js
+++ exercise2-4/sketch.js
@@ -7,6 +7,11 @@ function draw() {
     background(0);
     for (let x = 0; x < width; x += 50) {
         for (let y = 0; y < height; y += 50) { 
+            if(x % 100 === 0 && y % 100 === 0 || x % 100 === 50 && y % 100 === 50){
+                fill (0);
+            } else {
+                fill(255);
+            }
             square(x, y, 50);
         }
     }
